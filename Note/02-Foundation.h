4821c39a4953ac70749db331bdff8cab3d55a10c
kongzhy <kongzhaoyang77@icloud.com>
2016-03-16 23:31:47 +0800

    foundation

---
+++ Note/02-Foundation.h
@@ -0,0 +1,74 @@
+//
+if ...:
+else...:
+elif...:
+for ... in:
+while:
+
+// String
+// 获取字符的编码表述
+ord()
+
+// 编码转换为对应的字符
+chr()
+
+// 编码为指定的bytes
+encode()
+
+// 解码bytes
+decode()
+
+// 字符或字节的长度
+len()
+
+//把字符串转换成整数
+int()
+
+// 替换字符
+replace()
+
+// List
+// 追加元素到末尾
+append()
+
+// 插入元素
+insert()
+
+// 删除末尾元素
+pop()
+
+// 生成整数序列
+range()
+
+// 转换为List
+list()
+
+// 获取索引
+index()
+
+// 排序
+sort()
+
+// Tuple
+
+// Dict / Dictionary / Map
+// 判断Key是否存在
+in
+get()
+
+// 删除元素
+pop()
+
+// Set
+// 添加元素
+add()
+
+// 删除元素
+remove()
+
+// 交集
+&
+
+// 并集
+|
+
